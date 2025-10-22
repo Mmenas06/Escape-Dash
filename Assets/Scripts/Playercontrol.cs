4e70b7cb9129bd304238f6863c2bcfb7f795aa83
Manuel Mena <mmenas01@iescastelar.com>
2025-10-22 10:08:28 +0200

    ver1

--- Assets/Scripts/Playercontrol.cs
+++ Assets/Scripts/Playercontrol.cs
@@ -1,16 +1,26 @@
+using System;
 using UnityEngine;
 //Manuel Mena
 public class Playercontrol : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the MonoBehaviour is 
+    
+    private Rigidbody2D rb;
+    private float horizontal;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
         
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal, rb.linearVelocity.y);
     }
 }
