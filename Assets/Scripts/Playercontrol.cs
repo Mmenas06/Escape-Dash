d526963fe0d20adebcefb8867255dac5d18f1c07
Manuel Mena <mmenas01@iescastelar.com>
2025-10-29 10:04:26 +0100

    ver 2
    Cambios:
    - Se le agrego movimiento y salto al personaje
    - Orientación del personaje según la dirección a la que se mueve
    - Creación del prefab del personaje

--- Assets/Scripts/Playercontrol.cs
+++ Assets/Scripts/Playercontrol.cs
@@ -5,22 +5,40 @@ public class Playercontrol : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is 
     
-    private Rigidbody2D rb;
-    private float horizontal;
+    public float speed = 5;
+    private Rigidbody2D rb2d;
+
+    private float move;
+    public float jumpForce = 4;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundRadius = 0.1f;
+    public LayerMask groundLayer;
+    
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        
+        move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
+
+        if (move != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(move),1,1);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jumpForce);
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontal, rb.linearVelocity.y);
+        isGrounded =  Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
 }
